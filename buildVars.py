bd1fc8b4c63928c0cf10d1a49acdf3fab5f82a87
Javi Dominguez <javidominguez_github@outlook.es>
2019-02-09 20:48:43 +0100

    2019.1 compatibility. Dev features to stable.

--- buildVars.py
+++ buildVars.py
@@ -19,13 +19,19 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("Provides accessibility features for Mozilla Firefox and Thunderbird"),
 	# version
-	"addon_version" : "1.6dev",
+	"addon_version" : "1.7",
 	# Author(s)
 	"addon_author" : u"Javi Dominguez <fjavids@gmail.com>",
 	# URL for the add-on documentation support
 	"addon_url" : 'https://github.com/javidominguez/MozillaScripts/releases',
 	# File name for the add-on help file.
-	"addon_docFileName" : "readme.html"
+	"addon_docFileName" : "readme.html",
+	# Minimum NVDA version supported (e.g. "2018.3")
+	"addon_minimumNVDAVersion" : "2018.1.0",
+	# Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
+	"addon_lastTestedNVDAVersion" : "2019.1.0",
+	# Add-on update channel (default is stable or None)
+	"addon_updateChannel" : "stable"
 }
 
 
