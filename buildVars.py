3fe7089b52f054bb679c0be4b3d7d48998111271
Javi Dominguez <javidominguez_github@outlook.es>
2021-05-30 12:58:35 +0200

    Tested in NVDA 2021.1: Compatibility OK.

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("Provides accessibility features for Mozilla Firefox and Thunderbird"),
 	# version
-	"addon_version" : "1.13",
+	"addon_version" : "1.14",
 	# Author(s)
 	"addon_author" : u"Javi Dominguez <fjavids@gmail.com>",
 	# URL for the add-on documentation support
@@ -29,7 +29,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3")
 	"addon_minimumNVDAVersion" : "2018.1.0",
 	# Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2021.0.0",
+	"addon_lastTestedNVDAVersion" : "2021.1.0",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel" : None
 }
