aca90d0cffeb464b0952d53f3a4a23aba0db4599
Javi Domínguez <fjavids@gmail.com>
2025-06-19 16:46:52 +0200

    Fixed bug in manifest
    Signed-off-by: Javi Domínguez <fjavids@gmail.com>

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Provides accessibility features for Mozilla Firefox and Thunderbird."""),
 	# version
-	"addon_version": "2025.1.0",
+	"addon_version": "2025.1.1",
 	# Author(s)
 	"addon_author": "Javi Dominguez <fjavids@gmail.com>",
 	# URL for the add-on documentation support
@@ -37,7 +37,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2023.1.0",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2024.1.1",
+	"addon_lastTestedNVDAVersion": "2025.1.1",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
